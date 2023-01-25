5f87333a6ab96e224af39b82fa42709629e575d1
Daniel Shechter <dshctr1993@gmail.com>
2023-01-26 00:01:30 +0200

    deploying to render 2

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socketIoServerUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3001/' : ''
+const socketIoServerUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3001/' : 'https://syncode-bhy4.onrender.com/'
 
 export const socket = io(socketIoServerUrl);
 export const WebsocketContext = createContext<Socket>(socket);
