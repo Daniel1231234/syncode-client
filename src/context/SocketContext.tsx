caab67bfbbe7dd6a1d9ab85283b06bdedc00a034
Daniel Shechter <dshctr1993@gmail.com>
2023-02-02 17:12:34 +0200

    Finished socket issues

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const socketIoServerUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3001/' : 'https://syncode-bhy4.onrender.com/'
+const socketIoServerUrl = import.meta.env.MODE === 'development' ? 'http://localhost:3001/' : ""
 
 export const socket = io(socketIoServerUrl);
 export const WebsocketContext = createContext<Socket>(socket);
